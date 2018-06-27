e203a34c57cbb797f55baad037a713572dfd02f6
Rehan Parkar <parkarcamp@gmail.com>
2018-06-26 22:21:58 -0500

    configured tableview with mock data

--- Repositer-Rx/Controller/TrendingViewController.swift
+++ Repositer-Rx/Controller/TrendingViewController.swift
@@ -11,15 +11,41 @@ import UIKit
 class TrendingViewController: UIViewController {
     
     //outlets
-    
     @IBOutlet weak var tableView: UITableView!
     
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        tableView.dataSource = self
+        tableView.delegate = self 
+        tableView.reloadData()
+        
     }
 
 
 }
 
+
+
+extension TrendingViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "trendingRepoCell", for: indexPath) as? TrendingRepoCell else {return UITableViewCell()}
+        
+        let repo = Repo(name: "SWIFT", description: "Apples swift language", image: UIImage(named: "searchIconLarge")!, noOfForks: 342, language: "Swift", noOfContributer: 999, repoUrl: "www.apple.com")
+        
+        cell.configureCell(repo: repo)
+        return cell
+    }
+}
