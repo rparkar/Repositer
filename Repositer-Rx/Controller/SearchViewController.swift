8a4f027bb9b97ac02a67ea86ce74a81251d216aa
Rehan Parkar <parkarcamp@gmail.com>
2018-06-26 20:29:40 -0500

    created tab bar controller and app assets

---
+++ Repositer-Rx/Controller/SearchViewController.swift
@@ -0,0 +1,19 @@
+//
+//  SearchViewController.swift
+//  Repositer-Rx
+//
+//  Created by Rehan Parkar on 2018-06-26.
+//  Copyright © 2018 Rehan Parkar. All rights reserved.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
--- Repositer-Rx/Controller/TrendingViewController.swift
+++ Repositer-Rx/Controller/TrendingViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  TrendingViewController.swift
 //  Repositer-Rx
 //
 //  Created by Rehan Parkar on 2018-06-26.
@@ -8,16 +8,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class TrendingViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
 
