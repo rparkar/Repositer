a6682bd33ea09f92724b5d7797623bd3393c133a
Rehan Parkar <parkarcamp@gmail.com>
2018-06-26 21:01:16 -0500

    created tableview for trending VC

--- Repositer-Rx/Controller/TrendingViewController.swift
+++ Repositer-Rx/Controller/TrendingViewController.swift
@@ -9,6 +9,11 @@
 import UIKit
 
 class TrendingViewController: UIViewController {
+    
+    //outlets
+    
+    @IBOutlet weak var tableView: UITableView!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
