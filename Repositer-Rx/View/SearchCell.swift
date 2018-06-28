a91994e3e27349a929f8f5ffc2cfa1a369512449
Rehan Parkar <parkarcamp@gmail.com>
2018-06-28 18:44:43 -0500

    created and configured searchCell

---
+++ Repositer-Rx/View/SearchCell.swift
@@ -0,0 +1,41 @@
+//
+//  SearchCell.swift
+//  Repositer-Rx
+//
+//  Created by Rehan Parkar on 2018-06-28.
+//  Copyright © 2018 Rehan Parkar. All rights reserved.
+//
+
+import UIKit
+
+class SearchCell: UITableViewCell {
+
+    //outlets
+    
+    @IBOutlet weak var backView: UIView!
+    @IBOutlet weak var repoNameLabel: UILabel!
+    @IBOutlet weak var repoDescriptionLabel: UILabel!
+    @IBOutlet weak var forkCountsLabel: UILabel!
+    @IBOutlet weak var languageLabel: UILabel!
+    @IBOutlet weak var searchImage: UIImageView!
+    
+    //variables
+    private var repoUrl: String?
+    
+    func configureCell(repo: Repo) {
+        repoNameLabel.text = repo.name
+        repoDescriptionLabel.text = repo.descripton
+        forkCountsLabel.text = String(describing:repo.numberOfForks)
+        languageLabel.text = repo.language
+        repoUrl = repo.repoUrl
+    }
+    
+        
+    override func awakeFromNib() {
+        super.awakeFromNib()
+
+        backView.layer.cornerRadius = 15
+        
+    }
+
+}
